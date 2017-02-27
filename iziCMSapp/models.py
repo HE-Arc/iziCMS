41324688161d3e93f7f0efe4e41b6ce3d92b68b8
julien-baumgartner <Julien.Baumgartner@he-arc.ch>
2017-02-27 13:34:10 +0100

    lecture de la BDD sur une view

--- iziCMSapp/models.py
+++ iziCMSapp/models.py
@@ -1,15 +1,22 @@
 from django.db import models
 from django.conf import settings
 
-# Create your models here.
 class Site(models.Model):
     name = models.CharField(max_length=200)
     url = models.CharField(max_length=200)
+    FTPHost = models.CharField(max_length=200, default="")
+    FTPId = models.CharField(max_length=200, default="127.0.0.1")
+    FTPPort = models.IntegerField(default=21)
     def __str__(self):
         return self.name
 
-
+#ManyToManyField?
 class UserRoleOnSite(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     site = models.ForeignKey(Site, on_delete=models.CASCADE)
-    isAdmin = models.BooleanField()
+
+class Page(models.Model):
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    path = models.CharField(max_length=200)
+    def __str__(self):
+        return self.path
