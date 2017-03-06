16353b636d7a05cdb83eb8ae994cc291b0fb49b8
julien-baumgartner <Julien.Baumgartner@he-arc.ch>
2017-03-06 13:05:31 +0100

    correction DB

--- iziCMSapp/models.py
+++ iziCMSapp/models.py
@@ -5,7 +5,7 @@ class Site(models.Model):
     name = models.CharField(max_length=200, default="")
     url = models.CharField(max_length=200, default="")
     FTPHost = models.CharField(max_length=200, default="")
-    FTPId = models.CharField(max_length=200, default="127.0.0.1")
+    FTPId = models.CharField(max_length=200, default="")
     FTPPort = models.IntegerField(default=21)
     users = models.ManyToManyField(settings.AUTH_USER_MODEL)
     def __str__(self):
@@ -15,7 +15,7 @@ class Page(models.Model):
     site = models.ForeignKey(Site, on_delete=models.CASCADE)
     path = models.CharField(max_length=200, default="")
     link = models.CharField(max_length=200, default="")
-    selector = models.CharField(max_length=200, default="")
+    selector = models.CharField(max_length=200, blank=True, default="")
 
     def __str__(self):
         return self.path
