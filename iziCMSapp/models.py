801ef08e75d72633580520762672fbea20dc989e
julien-baumgartner <Julien.Baumgartner@he-arc.ch>
2017-03-06 13:58:26 +0100

    UrlField

--- iziCMSapp/models.py
+++ iziCMSapp/models.py
@@ -3,8 +3,8 @@ from django.conf import settings
 
 class Site(models.Model):
     name = models.CharField(max_length=200, default="")
-    url = models.CharField(max_length=200, default="")
-    FTPHost = models.CharField(max_length=200, default="")
+    url = models.URLField(default="")
+    FTPHost = models.URLField(default="")
     FTPId = models.CharField(max_length=200, default="")
     FTPPort = models.IntegerField(default=21)
     users = models.ManyToManyField(settings.AUTH_USER_MODEL)
@@ -14,7 +14,7 @@ class Site(models.Model):
 class Page(models.Model):
     site = models.ForeignKey(Site, on_delete=models.CASCADE)
     path = models.CharField(max_length=200, default="")
-    link = models.CharField(max_length=200, default="")
+    link = models.URLField(max_length=200, default="")
     selector = models.CharField(max_length=200, blank=True, default="")
 
     def __str__(self):
