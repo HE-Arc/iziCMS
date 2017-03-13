461d1a1ca81bea0591330f1fba442252a5abbb1a
julien-baumgartner <Julien.Baumgartner@he-arc.ch>
2017-03-13 14:00:44 +0100

    lecture fichier FTP dans textarea

--- iziCMSapp/urls.py
+++ iziCMSapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^testFTP/', views.testFTP, name='testFTP'),
+    url(r'^submitFTP/', views.testFTP, name='submitFTP'),
 ]
