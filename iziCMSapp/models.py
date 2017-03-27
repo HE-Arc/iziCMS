f1288c72e4652b79dc870b220d5233912cf1c23e
joaquim <joaquim.perez@he-arc.ch>
2017-03-27 22:21:10 +0200

    modification des modèles, faire makemigration et migrate

--- iziCMSapp/models.py
+++ iziCMSapp/models.py
@@ -3,18 +3,20 @@ from django.conf import settings
 
 class Site(models.Model):
     name = models.CharField(max_length=200, default="")
-    url = models.URLField(default="")
     ftp_host = models.CharField(max_length=200, default="")
     ftp_user = models.CharField(max_length=200, default="")
     ftp_port = models.IntegerField(default=21)
-    users = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    url = models.URLField(default="")
+
+    class Meta:
+        unique_together = ('ftp_host', 'ftp_user')
+
     def __str__(self):
         return self.name
 
 class Page(models.Model):
     site = models.ForeignKey(Site, on_delete=models.CASCADE)
     path = models.CharField(max_length=200, default="")
-    link = models.URLField(max_length=200, default="")
     selector = models.CharField(max_length=200, blank=True, default="")
 
     def __str__(self):
