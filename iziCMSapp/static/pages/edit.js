8fd95396536cdc3b3f0f2b70dfaaaad6614c5bfa
julien-baumgartner <julien.baumgartner@he-arc.ch>
2017-04-05 17:25:58 +0200

    modification d'une seule div de la page html au lieu de toute la page

--- iziCMSapp/static/pages/edit.js
+++ iziCMSapp/static/pages/edit.js
@@ -1,5 +1,17 @@
-var quill = new Quill('#iziCMSeditor', {theme: 'snow'});
+//document.getElementById('edition').style.visibility = 'visible'
+
+var quill = new Quill('#iziCMSeditor', {
+    theme: 'snow',
+    modules: {
+        toolbar: true
+      }
+    });
+
+
+//document.getElementsByClassName('ql-toolbar ql-snow').style.visibility = 'visible'
 
 document.querySelector("form").onsubmit = function(){
-  document.getElementById("pageContent").value = document.querySelector(".ql-editor").innerHTML;
+    //document.getElementById("edition").innerHTML  = document.querySelector(".ql-editor").innerHTML;
+    //document.getElementById("pageContent").value = document.getElementById("iziCMSeditor").innerHTML;
+    document.getElementById("editContent").value = document.querySelector(".ql-editor").innerHTML;
 };
--- iziCMSapp/views.py
+++ iziCMSapp/views.py
@@ -5,6 +5,8 @@ import logging
 
 from django.http import HttpResponse
 from .models import Site
+from bs4 import BeautifulSoup
+import html
 
 logger = logging.getLogger(__name__)
 
@@ -50,10 +52,12 @@ def pages_edit(request, website_id, page_id):
     pwd = request.POST['password']
     file = FTPManager.download(site.ftp_host, site.ftp_port, site.ftp_user, pwd, "", page.path)
 
+    soup = BeautifulSoup(file, "html.parser")
+    tag = soup.find(id="iziEdit")
     template = loader.get_template('pages/edit.html')
 
     return HttpResponse(
-        template.render({'site':site,'page':page, 'file':file}, request))
+        template.render({'site':site,'page':page, 'file':file, 'tag':str(tag)}, request))
 
 def pages_add(request, website_id):
     site = Site.objects.get(id=website_id)
@@ -68,8 +72,17 @@ def pages_update(request, website_id, page_id):
 
     pwd = request.POST['password']
     pageContent = request.POST['pageContent']
+    editContent = request.POST['editContent']
 
-    FTPManager.upload(site.ftp_host, site.ftp_port, site.ftp_user, pwd, "", page.path, pageContent)
+    soup = BeautifulSoup(pageContent, "html.parser")
+
+    newDiv = soup.new_tag('div')
+    newDiv['id'] = 'iziEdit'
+    newDiv.string = editContent
+
+    soup.find(id="iziEdit").replace_with(newDiv)
+
+    FTPManager.upload(site.ftp_host, site.ftp_port, site.ftp_user, pwd, "", page.path, html.unescape(str(soup)))
 
     return redirect('pages_index', website_id=site.id)
 
