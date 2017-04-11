2fd7fd221c2a86c877840a38444aeef0e4e83c9d
joaquim <joaquim.perez@he-arc.ch>
2017-04-11 23:04:28 +0200

    charfield 255 pour apaiser les epsrits

--- iziCMSapp/models.py
+++ iziCMSapp/models.py
@@ -3,22 +3,22 @@ from django.conf import settings
 
 class Site(models.Model):
     # the "http" host of the website: www.exemple.com
-    hostname = models.CharField(max_length=200, default="", unique=True)
+    hostname = models.CharField(max_length=255, default="", unique=True)
     # the ftp host can be the same as well as totally different, common case: ftp.exemple.com
-    ftp_host = models.CharField(max_length=200, default="")
-    ftp_user = models.CharField(max_length=200, default="")
+    ftp_host = models.CharField(max_length=255, default="")
+    ftp_user = models.CharField(max_length=255, default="")
     ftp_port = models.IntegerField(default=21) # 21 in most cases
 
     # the base folder of the site in the ftp arborescence, not always /
-    root_folder = models.CharField(max_length=200, default="/")
+    root_folder = models.CharField(max_length=255, default="/")
 
     def __str__(self):
         return self.hostname
 
 class Page(models.Model):
     site = models.ForeignKey(Site, on_delete=models.CASCADE)
-    path = models.CharField(max_length=200, default="")
-    selector = models.CharField(max_length=200, default="body")
+    path = models.CharField(max_length=255, default="")
+    selector = models.CharField(max_length=255, default="body")
 
     def __str__(self):
         return self.path
